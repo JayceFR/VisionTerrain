7a5986fc99b06e152f5ef30e198798e4cd933da4
JayceFR <jaycejefferson.vicious@gmail.com>
2025-07-07 15:37:13 +0100

    started porting to web

--- main.c
+++ main.c
@@ -21,6 +21,11 @@
 
 #include <arpa/inet.h>
 
+#ifdef __EMSCRIPTEN__
+  #include <emscripten/emscripten.h>
+#endif
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
   glViewport(0, 0, width, height);
 }
